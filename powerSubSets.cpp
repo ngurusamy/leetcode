0479d880851f8f4688b69928edb130ac28e4eb08
ngurusamy <gurusow2010@live.com>
2020-07-22 22:36:29 -0400

    Create powerSubSets.cpp

---
+++ powerSubSets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    void subSets(vector<int> & nums, int idx, vector<vector<int>> & results)
+    {
+        if(idx == nums.size() - 1) {
+            results.push_back({});
+            results.push_back({nums[idx]});
+        }
+        else
+        {
+            subSets(nums, idx + 1, results);
+            int n = results.size();
+            for(int i = 0; i < n; i++) 
+            {
+                vector<int> r = results[i];
+                r.push_back(nums[idx]);
+                results.push_back(r);
+            }
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        if(nums.empty())
+            return {{}};
+        vector<vector<int>> res;
+        
+        subSets(nums, 0, res);
+        
+        return res;
+    }
+};
