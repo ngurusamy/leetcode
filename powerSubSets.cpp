e5d94e6615b5fc1934d3580ea1f9e9f513be8f9f
ngurusamy <gurusow2010@live.com>
2020-07-22 22:50:02 -0400

    Add Iterative method of solving

--- powerSubSets.cpp
+++ powerSubSets.cpp
@@ -29,4 +29,24 @@ public:
         
         return res;
     }
+    
+        vector<vector<int>> subsets_itrMode(vector<int>& nums) {
+        
+        if(nums.empty())
+            return {{}};
+        vector<vector<int>> res = {{}};
+        
+        //subSets(nums, 0, res);
+        for(int x : nums) {
+            int n = res.size();
+            for(int i = 0; i < n; i++) {
+                vector<int> r = res[i];
+                r.push_back(x);
+                res.push_back(r);
+            }
+        }
+        
+        
+        return res;
+    }
 };
