632160973a199680b85da3763120e901c9cb955e
ngurusamy <gurusow2010@live.com>
2020-07-25 01:51:00 -0400

    Create topKFrequentElements.java

---
+++ java/topKFrequentElements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        int[] result = new int[k];
+        
+        Map<Integer, Integer> counts = new HashMap<>();
+        
+        for(int x : nums) {
+            counts.put(x, counts.getOrDefault(x, 0) + 1);
+        }
+        
+        Queue<Integer> PQ = new PriorityQueue<>((a, b) -> counts.get(b) - counts.get(a));
+        
+        for(int x : counts.keySet())
+            PQ.add(x);
+        
+        for(int i = 0; i < k; i++) {
+            result[i] = PQ.poll();
+        }
+        return result;
+    }
+}
