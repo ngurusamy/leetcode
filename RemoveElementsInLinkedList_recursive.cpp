74af8670b018a87adae65e0aa2040344544170a5
ngurusamy <gurusow2010@live.com>
2020-07-27 15:16:43 -0400

    Create RemoveElementsInLinkedList_recursive.cpp

---
+++ RemoveElementsInLinkedList_recursive.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   /* ListNode* removeElements(ListNode* head, int val) {
+        
+        ListNode * current = head, * prev = NULL;
+        
+        if(head == NULL)
+            return head;
+        
+        while(current != NULL) {
+            
+            if(current->val == val){
+                if(prev == NULL) {
+                    head = current->next;
+                } else {
+                    prev->next = current->next;
+                }
+                ListNode * tmp = current;
+                current = current->next;
+                delete(tmp);
+            } 
+            else
+            {
+                prev = current;
+                current = current->next;
+            }            
+        }
+        return head;
+    }*/
+    
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL) return head;
+        
+        ListNode * h = removeElements(head->next, val);
+        
+        if(head->val == val) {
+            ListNode * tmp = head;
+            head = h;
+            delete(tmp);
+            return h;
+        } else {
+            head->next = h;
+        }
+        return head;
+    }
+    
+};
